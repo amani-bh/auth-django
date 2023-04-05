274fd9b69c5399eaad0d0b373346cdfe938e9e77
Amani Ben Hassine <amani.benhassine@esprit.tn>
2023-04-06 00:57:59 +0100

    update

--- auth/app/urls.py
+++ auth/app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import RegisterAPIView, LoginAPIView, UserAPIView, RefreshAPIView, LogoutAPIView, ActivationView
+from .views import RegisterAPIView, LoginAPIView, UserAPIView, RefreshAPIView, LogoutAPIView, GoogleLoginView, \
+    ActivationView, ImageUploadView, UpdateProfile, UpdatePassword
 
 urlpatterns = [
     path('register', RegisterAPIView.as_view()),
@@ -7,5 +8,9 @@ urlpatterns = [
     path('user', UserAPIView.as_view()),
     path('refresh', RefreshAPIView.as_view()),
     path('logout', LogoutAPIView.as_view()),
+    path('googleRegister', GoogleLoginView.as_view(), name='google_login_view'),
     path('activate/<uidb64>/<token>/', ActivationView.as_view(), name='activate'),
+    path('uploadImage', ImageUploadView.as_view()),
+    path('updateProfile', UpdateProfile.as_view()),
+    path('updatePassword', UpdatePassword.as_view()),
 ]
--- auth/app/views.py
+++ auth/app/views.py
@@ -1,6 +1,6 @@
 from django.conf import settings
-from django.utils.encoding import force_bytes
-from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from google.auth.transport import requests
+from google.oauth2 import id_token
 from rest_framework import status
 from rest_framework.authentication import get_authorization_header
 from rest_framework.response import Response
@@ -10,8 +10,11 @@ from rest_framework.exceptions import APIException, AuthenticationFailed
 from .authentication import create_access_token, create_refresh_token, decode_access_token, decode_refresh_token
 from .serializers import UserSerializer
 from .models import User
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from django.utils.encoding import force_bytes
 from django.core.mail import send_mail
 from django.urls import reverse
+
 from .tokens import account_activation_token
 
 
@@ -28,7 +31,7 @@ class RegisterAPIView(APIView):
 
         send_mail(
             'Activate your account',
-            f'Hi {user.first_name}, please activate your account by clicking on the link below: {activate_url}',
+            f'Hi {user.first_name} {user.last_name}, please activate your account by clicking on the link below: {activate_url}',
             settings.DEFAULT_FROM_EMAIL,
             [user.email],
             fail_silently=False,
@@ -43,6 +46,8 @@ class LoginAPIView(APIView):
 
         if not user:
             raise APIException('Invalid credentials!')
+        if not user.is_active:
+            raise APIException('Verify your mail !')
 
         if not user.check_password(request.data['password']):
             raise APIException('Invalid credentials!')
@@ -95,6 +100,52 @@ class LogoutAPIView(APIView):
         return response
 
 
+class GoogleLoginView(APIView):
+    def post(self, request):
+        id_token1 = request.data.get('id_token')
+        if not id_token1:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            # Specify the CLIENT_ID of the app that accesses the backend:
+            idinfo = id_token.verify_oauth2_token(id_token1, requests.Request(), '767545735730-dbak9de0g63e2n8ou2vnuefgm0c74jvq.apps.googleusercontent.com')
+
+            # Get user information from idinfo
+            email = idinfo['email']
+            first_name = idinfo.get('given_name', '')
+            last_name = idinfo.get('family_name', '')
+
+            # Check if user already exists
+            try:
+                user = User.objects.get(email=email)
+            except User.DoesNotExist:
+                # Create new user
+                 serializer = UserSerializer(data={
+                        "email": email,
+                        "first_name": first_name,
+                        "last_name": last_name,
+                        "phone":"21333",
+                        "password":"123"
+                    })
+                 serializer.is_valid(raise_exception=True)
+                 user=serializer.save()
+
+            # Set is_active to True
+            user.is_active = True
+            user.save()
+            # Generate JWT token and send in response
+            access_token = create_access_token(user.id)
+            refresh_token = create_refresh_token(user.id)
+            response = Response()
+            response.set_cookie(key='refreshToken', value=refresh_token, httponly=True)
+            response.data = {'token': access_token}
+            return response
+
+        except ValueError:
+        # Invalid token
+           return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
 # activate account
 class ActivationView(APIView):
     def get(self, request, uidb64, token):
@@ -110,3 +161,50 @@ class ActivationView(APIView):
             return Response({'message': 'Activation successful!'}, status=status.HTTP_200_OK)
 
         return Response({'message': 'Activation link is invalid!'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ImageUploadView(APIView):
+    def post(self, request):
+        image_url = request.data.get('image_url')
+        id = request.data.get('id')
+        try:
+            user = User.objects.filter(pk=id).first()
+            user.image_url=image_url
+            user.save()
+            return Response({'message': 'Image uploaded successfully!'}, status=status.HTTP_200_OK)
+        except User.DoesNotExist:
+            return Response({'error': 'user not found'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UpdateProfile(APIView):
+    def post(self, request):
+        first_name = request.data.get('first_name')
+        id = request.data.get('id')
+        last_name = request.data.get('last_name')
+        phone = request.data.get('phone')
+        email = request.data.get('email')
+        try:
+            user = User.objects.filter(pk=id).first()
+            user.first_name = first_name
+            user.last_name = last_name
+            user.phone = phone
+            user.email = email
+            user.save()
+            return Response({'message': 'User updated successfully!'}, status=status.HTTP_200_OK)
+        except User.DoesNotExist:
+            return Response({'error': 'user not found'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UpdatePassword(APIView):
+    def post(self, request):
+        id = request.data.get('id')
+        password = request.data.get('password')
+        try:
+            user = User.objects.filter(pk=id).first()
+            serializer=UserSerializer(user, data={'password': password}, partial=True)
+            serializer.is_valid(raise_exception=True)
+            serializer.update_password(user, serializer.validated_data)
+            return Response({'message': 'Password updated successfully!'}, status=status.HTTP_200_OK)
+        except User.DoesNotExist:
+            return Response({'error': 'user not found'}, status=status.HTTP_400_BAD_REQUEST)
+
